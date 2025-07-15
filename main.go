37e68f17a6d3ee0ef8a586e3e48b92eafa32d57e
IhsanAlhakim <ihsanalhakim04@gmail.com>
2025-07-15 10:59:53 +0700

    disable godotenv.Load()

--- main.go
+++ main.go
@@ -11,15 +11,14 @@ import (
 	"backend/handler"
 
 	"github.com/gorilla/context"
-	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Failed to load environment configuration file: %v ", err.Error())
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Failed to load environment configuration file: %v ", err.Error())
+	// }
 	
 	PORT := os.Getenv("PORT")
 	
