d0a87a63d4fdb8c12ace91631b018a188d46150c
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-01-28 12:12:57 +0700

    feat: add config struct and loader for environment variables

---
+++ internal/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	SessionID             string
+	MongoConnectionString string
+	SessionAuthKey        string
+	SessionEncryptionKey  string
+}
+
+func Load() *Config {
+	if os.Getenv("ENV") != "production" {
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found, continue using system environment variables")
+		}
+	}
+
+	return &Config{
+		SessionID:             os.Getenv("SESSION_ID"),
+		MongoConnectionString: os.Getenv("MONGO_CONNECTION_STRING"),
+		SessionAuthKey:        os.Getenv("SESSION_AUTH_KEY"),
+		SessionEncryptionKey:  os.Getenv("SESSION_ENCRYPTION_KEY"),
+	}
+}
